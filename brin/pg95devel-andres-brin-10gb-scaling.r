655d46b028bca5ae85a87582c9aa316ddf3e10f8
Mark Wong <mark@2ndQuadrant.com>
2014-11-24 14:26:21 -0800

    Add chart for brin baseline results

---
+++ brin/pg95devel-andres-brin-10gb-scaling.r
@@ -0,0 +1,17 @@
+# R --slave --no-save < pg95devel-andres-brin-10gb-scaling.r
+
+df <- read.csv("pg95devel-andres-brin-10gb-scaling.csv", header=T)
+
+# https://wiki.postgresql.org/wiki/Color_Palette
+color="#0064a5"
+
+bitmap("pg95devel-andres-brin-10gb-scaling.png", type="png16m", units="px",
+       width=888, height=555, res=150, taa=4, gaa=4)
+
+plot(df$streams, df$throughput, col=color, type="b",
+     ylim=c(0, max(df$throughput)),
+     main="DBT3 PostgreSQL 9.5devel (andres&brin) Throughput Test Scaling",
+     xlab="Number of Streams",
+     ylab="Numerical Quanitity")
+grid(col="gray")
+dev.off()
