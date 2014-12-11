c11c1d70cbe70d20dffc1d9da7638401efe063e4
Mark Wong <mark@2ndQuadrant.com>
2014-12-11 11:10:32 -0800

    Add more brin charts

--- brin/pg95devel-andres-brin-10gb-scaling.r
+++ brin/pg95devel-andres-brin-10gb-scaling.r
@@ -1,17 +1,25 @@
 # R --slave --no-save < pg95devel-andres-brin-10gb-scaling.r
 
-df <- read.csv("pg95devel-andres-brin-10gb-scaling.csv", header=T)
+df10 <- read.csv("pg95devel-andres-brin-10gb-scaling.csv", header=T)
+df30 <- read.csv("pg95devel-andres-brin-30gb-scaling.csv", header=T)
+
+colors <- rainbow(2)
+pch <- seq.int(1, 2)
 
 # https://wiki.postgresql.org/wiki/Color_Palette
-color="#0064a5"
+#color="#0064a5"
 
 bitmap("pg95devel-andres-brin-10gb-scaling.png", type="png16m", units="px",
        width=888, height=555, res=150, taa=4, gaa=4)
 
-plot(df$streams, df$throughput, col=color, type="b",
-     ylim=c(0, max(df$throughput)),
+plot(df10$streams, df10$throughput, col=colors[1], type="b", pch=pch[1],
+     ylim=c(0, max(df10$throughput)),
      main="DBT3 PostgreSQL 9.5devel (andres&brin) Throughput Test Scaling",
      xlab="Number of Streams",
      ylab="Numerical Quanitity")
+points(df30$streams, df30$throughput, col=colors[2], type="b", pch=pch[2])
+
+legend('bottomright', c("10GB", "30GB"), col=colors, pch=pch)
+
 grid(col="gray")
 dev.off()
